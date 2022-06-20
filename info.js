b7a4900c805b693bd44331aa0853e587c9bfc0c8
chukwujike <79435086+chukwujike@users.noreply.github.com>
2022-06-20 17:15:01 +0200

    Add files via upload

---
+++ info.js
@@ -0,0 +1,6 @@
+const name_student = "Ezemandu Chukwujike"
+const height = "185cm"
+const nationality = "Nigerian"
+console.log("Name: " + name_student)
+console.log("Height: " + height)
+console.log("Nationality: " + nationality)
